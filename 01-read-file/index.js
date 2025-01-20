8e556d1f7cbc700b5f0715b9de5e90fdcb64cba2
Eugeku <eugenekuzora@gmail.com>
2025-01-20 12:28:27 +0300

    feat: implement error handler for 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -5,3 +5,7 @@ const filePath = path.join(__dirname, './text.txt');
 const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
 
 readStream.pipe(process.stdout);
+
+readStream.on('error', (error) => {
+    console.error(`Error: ${error.message}`);
+});
